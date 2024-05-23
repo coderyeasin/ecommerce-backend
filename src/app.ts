5cd42814e088d15ccc45fcdbbe349f1d5aee32a8
coderyeasin <yeasin3233@gmail.com>
2024-05-23 18:11:07 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { EProductRouter } from "./modules/products/product.route";
 import { EOrderRouter } from "./modules/orders/order.route";
@@ -11,5 +11,12 @@ app.use(cors());
 
 app.use("/api/products", EProductRouter);
 app.use("/api/orders", EOrderRouter);
+
+// just a welcome msg to show
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        message:'E-Commerce API Service'
+    })
+})
 
 export default app;
